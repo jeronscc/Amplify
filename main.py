3ccb7ed776375c6bda190541ac15494a6472b9a0
JustinRyanFlores <112955705+JustinRyanFlores@users.noreply.github.com>
2022-12-04 02:45:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,3 +61,35 @@ resized = resume.resize((50,50))
 resume = ImageTk.PhotoImage(resized)
 Button(root, image=resume, bg= "#191414", bd=0, command = mixer.music.unpause).place(x=50,y=560)
 
+#Stop
+stop = Image.open("buttons/stop.png")
+resized = stop.resize((50,50))
+stop = ImageTk.PhotoImage(resized)
+Button(root, image=stop, bg= "#191414", bd=0, command = mixer.music.stop).place(x=245,y=560)
+
+#Music Name
+music=Label(root,text="",font=("Krona",15), fg ="white", bg="#191414")
+music.place(x=170,y=530,anchor= "center")
+
+#Playlist Background
+cover = Image.open("background.png")
+resized = cover.resize((249,275))
+menu = ImageTk.PhotoImage(resized)
+Label(root, image=menu, bg= "#191414").place(x=50,y=210)
+
+#Frame
+musicframe = Frame(root,bd = 2,relief=RIDGE)
+musicframe.place(x=61,y=250,width=230,height=230)
+
+#Open Folder icon
+Button(root, text="Open Folder", width = 10 , height = 1, font =("Arial",10,"bold"), fg= "White", bg="#1ed760", command=openFolder).place(x=62, y=217)
+
+#Scrollbar
+scroll = Scrollbar(musicframe)
+playlist = Listbox(musicframe,width = 100, bg="#525252",fg="White", selectbackground="#191414",cursor= "hand2", bd=0, yscrollcommand=scroll.set)
+scroll.config(command=playlist.yview)
+scroll.pack(side=RIGHT, fill = Y)
+playlist.pack(side=LEFT, fill = BOTH)
+
+root.mainloop()
+
